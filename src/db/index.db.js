0553c93a31781bb04426af32a97e268680e98c64
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-07-01 12:15:51 +0600

    mongodb connection setup ready

---
+++ src/db/index.db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionOnInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}`,
+    );
+
+    console.log(
+      `\n MongoDB Connected: ${connectionOnInstance.connection.host}`,
+    );
+  } catch (error) {}
+  console.log(`Catch the error here: ${error}`);
+  process.exit(1);
+};
